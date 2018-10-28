71f2ef902222d772b576f991c92e157f633a3c95
Thodoris Pliakas <thodorispliakas@gmail.com>
2018-10-29 00:08:05 +0200

    updated package to use nodemon for auto-reloading

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const app = express();
 
-app.get("/", (req, res) => res.send("Hello!"));
+app.get("/", (req, res) => res.send("Hello there!"));
 
 // process.env.PORT will be needed by Heroku later
 const port = process.env.PORT || 5000;
