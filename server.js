64c90cccd26b868cdafa1a88b3ec50e490442644
theodoros <thodorispliakas@gmail.com>
2019-02-10 23:54:17 +0200

    Add routes

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const mongoose = require('mongoose');
 
+const users = require('./routes/api/users');
+const profile = require('./routes/api/profile');
+const posts = require('./routes/api/posts');
+
 const app = express();
 
 // DB config
@@ -14,7 +18,12 @@ mongoose
 
 app.get("/", (req, res) => res.send("Hello there!"));
 
+// Use Routes
+app.use('/api/users', users);
+app.use('/api/profile', profile);
+app.use('/api/posts', posts);
+
 // process.env.PORT will be needed by Heroku later
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5001;
 
 app.listen(port, () => console.log(`Server is running on port ${port}.`));
