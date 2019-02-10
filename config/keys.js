6a96dbb6cb913e0623b6ee5124e125b63c5aadf6
theodoros <thodorispliakas@gmail.com>
2019-02-10 23:16:03 +0200

    Connection to DB

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI : 'mongodb://mongouser:Mongouser123@ds143893.mlab.com:43893/mongodata'
+};
--- server.js
+++ server.js
@@ -1,6 +1,16 @@
 const express = require("express");
+const mongoose = require('mongoose');
 
 const app = express();
+
+// DB config
+const db = require('./config/keys').mongoURI;
+
+// Connect to MongoDB
+mongoose
+    .connect(db, { useNewUrlParser: true })
+    .then(() => console.log('MongoDB Connected'))
+    .catch(err => console.log(err));
 
 app.get("/", (req, res) => res.send("Hello there!"));
 
