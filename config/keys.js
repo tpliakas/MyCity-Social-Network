8fe3c52bc98d3f624860768fc0b9af7492701aee
Theodore <thodorispliakas@gmail.com>
2019-12-03 00:47:23 +0200

    Start fresh

--- config/keys.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-    mongoURI : 'mongodb://mongouser:Mongouser123@ds143893.mlab.com:43893/mongodata'
-};
--- routes/api/posts.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-// @route     GET api/posts/test
-// @desc      Tests post route
-// @access    Public
-router.get('/test', (req, res) => res.json({ mas: 'Post Works'}));
-
-module.exports = router;
