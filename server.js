e55ef7f9de225ef23ccd3034712df59ea6a20b64
Theodore <thodorispliakas@gmail.com>
2019-12-16 23:31:23 +0200

    Added basic routes

--- server.js
+++ server.js
@@ -8,6 +8,12 @@ connectDB();
 
 app.get('/', (req, res) => res.send('API Running'));
 
+// Define routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
+
 // Port for heroku deployment
 const PORT = process.env.PORT || 5000;
 
