1dc9257e1c48c6aca16b1c3f8cc5603698461a08
Theodore <thodorispliakas@gmail.com>
2020-01-22 23:27:07 +0200

    Created token

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 const { check, validationResult } = require('express-validator');
 
 const User = require('../../models/User');
@@ -61,8 +63,21 @@ router.post(
             await user.save();
 
             // return jsonwebtoken
+            const payload = {
+                user: {
+                    id: user.id //Mongoose uses abstraction for _id in db
+                }
+            };
 
-            res.send('User registered');
+            jwt.sign(
+                payload,
+                config.get('jwtSecret'),
+                { expiresIn: 360000 },
+                (err, token) => {
+                    if (err) throw err;
+                    res.json({ token });
+                }
+            );
         } catch (err) {
             console.log(err.message);
             res.status(500).send('Server error');
