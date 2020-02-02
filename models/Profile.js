27f4d775e33e86ab58bcc8e0d9a6c02836a49b8a
Theodore <thodorispliakas@gmail.com>
2020-02-03 00:10:17 +0200

    Profile model

---
+++ models/Profile.js
@@ -0,0 +1,51 @@
+const mongoose = require('mongoose');
+
+const ProfileSchema = new mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'user'
+    },
+    company: {
+        type: String
+    },
+    website: {
+        type: String
+    },
+    location: {
+        type: String
+    },
+    status: {
+        type: String,
+        required: true
+    },
+    skills: {
+        type: [String],
+        required: true
+    },
+    bio: {
+        type: String
+    },
+    social: {
+        youtube: {
+            type: String
+        },
+        twitter: {
+            type: String
+        },
+        facebook: {
+            type: String
+        },
+        linkedin: {
+            type: String
+        },
+        instagram: {
+            type: String
+        }
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.exports = Profile = mongoose.model('profile', ProfileSchema);
