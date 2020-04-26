cb681c782624d94753efe1ff709f97bc5c38e8c8
Theodore <thodorispliakas@gmail.com>
2020-04-27 02:56:51 +0300

    profile route wip

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const auth = require('../../middleware/auth');
+const { check, validationResult } = require('express-validator/check');
 
 const Profile = require('../../models/Profile');
 const User = require('../../models/User');
@@ -26,5 +27,26 @@ router.get('/me', auth, async (req, res) => {
         res.status(500).send('Server Error');
     }
 });
+
+// @route   POST api/profile
+// @desc    Create or update user profile
+// @access  Private
+router.post(
+    '/',
+    [
+        check('status', 'Status is required')
+            .not()
+            .isEmpty(),
+        check('skills', 'Skills is required')
+            .not()
+            .isEmpty()
+    ],
+    async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+    }
+);
 
 module.exports = router;
