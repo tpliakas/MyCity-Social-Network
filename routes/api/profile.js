30d334ae064ba9f5bc93724f453641490e00e359
Theodore <thodorispliakas@gmail.com>
2020-06-22 01:11:21 +0300

    Created all profiles route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -106,4 +106,20 @@ router.post(
     }
 );
 
+// @route   POST api/profile
+// @desc    Get all profiles
+// @access  Public
+router.get('/', async (req, res) => {
+    try {
+        const profiles = await Profile.find().populate('user', [
+            'name',
+            'avatar'
+        ]);
+        res.json(profiles);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+});
+
 module.exports = router;
