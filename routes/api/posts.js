244f2fda146efe7df32823b63f7a74d261a0d891
Theodore <thodorispliakas@gmail.com>
2020-07-22 00:34:55 +0300

    Get post by id

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -51,4 +51,25 @@ router.get('/', auth, async (req, res) => {
     }
 });
 
+// @route    GET api/posts/:id
+// @desc     Get post by id
+// @access   Private
+router.get('/:id', auth, async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+
+        if (!post) {
+            return res.status(404).json({ msg: 'Post not found' });
+        }
+
+        res.json(post);
+    } catch (err) {
+        console.error(err.message);
+        if (err.kind === 'ObjectId') {
+            return res.status(404).json({ msg: 'Post not found' });
+        }
+        res.status(500).send('Server Error');
+    }
+});
+
 module.exports = router;
