f807e5e379476b9c745f7d319fcfd62a97643ce3
Theodore <thodorispliakas@gmail.com>
2020-07-22 00:44:41 +0300

    Delete post by id

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -72,4 +72,32 @@ router.get('/:id', auth, async (req, res) => {
     }
 });
 
+// @route    DELETE api/posts/:id
+// @desc     Delete a post
+// @access   Private
+router.delete('/:id', auth, async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+
+        if (!post) {
+            return res.status(404).json({ msg: 'Post not found' });
+        }
+
+        // Check user
+        if (post.user.toString() !== req.user.id) {
+            return res.status(401).json({ msg: 'User not authorized' });
+        }
+
+        await post.remove();
+
+        res.json({ msg: 'Post removed' });
+    } catch (err) {
+        console.error(err.message);
+        if (err.kind === 'ObjectId') {
+            return res.status(404).json({ msg: 'Post not found' });
+        }
+        res.status(500).send('Server Error');
+    }
+});
+
 module.exports = router;
