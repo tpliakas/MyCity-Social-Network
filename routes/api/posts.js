b1c9a5e2820295ea7ad8fa64ac9cc4f7a73efb2b
Theodore <thodorispliakas@gmail.com>
2020-07-27 00:44:40 +0300

    Like a post

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -100,4 +100,27 @@ router.delete('/:id', auth, async (req, res) => {
     }
 });
 
+// @route    PUT api/posts/like/:id
+// @desc     Like a post
+// @access   Private
+router.put('/like/:id', auth, async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+
+        // Check if the post has already been liked
+        if (post.likes.some((like) => like.user.toString() === req.user.id)) {
+            return res.status(400).json({ msg: 'Post already liked' });
+        }
+
+        post.likes.unshift({ user: req.user.id });
+
+        await post.save();
+
+        return res.json(post.likes);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+});
+
 module.exports = router;
