e62295cbc5058b5182aa9f2d639681249a17951c
Theodore <thodorispliakas@gmail.com>
2020-08-16 01:23:01 +0300

    Auth action

---
+++ client/src/actions/auth.js
@@ -0,0 +1,33 @@
+import axios from 'axios';
+import { setAlert } from './alert';
+import { REGISTER_SUCCESS, REGISTER_FAIL } from './types';
+
+// Register User
+export const register = ({ name, email, password }) => async (dispatch) => {
+  const config = {
+    header: {
+      'Content-Type': 'application/json'
+    }
+  };
+
+  const body = JSON.stringify({ name, email, password });
+
+  try {
+    const res = await axios.post('/api/users', body, config);
+
+    dispatch({
+      type: REGISTER_SUCCESS,
+      payload: res.data // This is the token returned
+    });
+  } catch (err) {
+    const errors = err.response.data.errors;
+
+    if (errors) {
+      errors.forEach((error) => dispatch(setAlert(error.msg, 'error')));
+    }
+
+    dispatch({
+      type: REGISTER_FAIL
+    });
+  }
+};
