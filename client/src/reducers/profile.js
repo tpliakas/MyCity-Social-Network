401728af6a9396f67e387a918344f83dacaec8d3
Theodore <thodorispliakas@gmail.com>
2020-08-31 01:32:26 +0300

    Added profile reducer

---
+++ client/src/reducers/profile.js
@@ -0,0 +1,30 @@
+import { GET_PROFILE, PROFILE_ERROR } from '../actions/types';
+
+const initialState = {
+  profile: null,
+  profiles: [],
+  loading: true,
+  error: {}
+};
+
+export default function (state = initialState, action) {
+  const { type, payload } = action;
+
+  switch (type) {
+    case GET_PROFILE:
+      return {
+        ...state,
+        profile: payload,
+        loading: false
+      };
+    case PROFILE_ERROR:
+      return {
+        ...state,
+        error: payload,
+        loading: false,
+        profile: null
+      };
+    default:
+      return state;
+  }
+}
