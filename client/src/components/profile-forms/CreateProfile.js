1ccf493b22b38483e010e30ac381f54e80ef6216
Theodore <thodorispliakas@gmail.com>
2020-09-02 23:21:26 +0300

    Social icon in create profile form

--- client/src/components/profile-forms/CreateProfile.js
+++ client/src/components/profile-forms/CreateProfile.js
@@ -181,79 +181,54 @@ const ProfileForm = ({
         >
           <Input.TextArea />
         </Form.Item>
+
+        <div className="my-2">
+          <button
+            onClick={() => toggleSocialInputs(!displaySocialInputs)}
+            type="button"
+            className="btn btn-light"
+          >
+            Add Social Network Links
+          </button>
+          <span>Optional</span>
+        </div>
+
+        {displaySocialInputs && (
+          <>
+            <Form.Item name="twitter">
+              <div className="form-group social-input">
+                <i className="fab fa-twitter fa-2x" />
+              </div>
+              <Input placeholder="Twitter URL" />
+            </Form.Item>
+            <Form.Item name="facebook">
+              <div className="form-group social-input">
+                <i className="fab fa-facebook fa-2x" />
+              </div>
+              <Input placeholder="Facebook URL" />
+            </Form.Item>
+            <Form.Item name="youtube">
+              <div className="form-group social-input">
+                <i className="fab fa-youtube fa-2x" />
+              </div>
+              <Input placeholder="Youtube URL" />
+            </Form.Item>
+            <Form.Item name="linkedin">
+              <div className="form-group social-input">
+                <i className="fab fa-linkedin fa-2x" />
+              </div>
+              <Input placeholder="Linkedin URL" />
+            </Form.Item>
+            <Form.Item name="instagram">
+              <div className="form-group social-input">
+                <i className="fab fa-instagram fa-2x" />
+              </div>
+              <Input placeholder="Instagram URL" />
+            </Form.Item>
+          </>
+        )}
       </Form>
-
-      <div className="my-2">
-        <button
-          onClick={() => toggleSocialInputs(!displaySocialInputs)}
-          type="button"
-          className="btn btn-light"
-        >
-          Add Social Network Links
-        </button>
-        <span>Optional</span>
-      </div>
-
-      {displaySocialInputs && (
-        <Fragment>
-          <div className="form-group social-input">
-            <i className="fab fa-twitter fa-2x" />
-            <input
-              type="text"
-              placeholder="Twitter URL"
-              name="twitter"
-              value={twitter}
-              onChange={onChange}
-            />
-          </div>
-
-          <div className="form-group social-input">
-            <i className="fab fa-facebook fa-2x" />
-            <input
-              type="text"
-              placeholder="Facebook URL"
-              name="facebook"
-              value={facebook}
-              onChange={onChange}
-            />
-          </div>
-
-          <div className="form-group social-input">
-            <i className="fab fa-youtube fa-2x" />
-            <input
-              type="text"
-              placeholder="YouTube URL"
-              name="youtube"
-              value={youtube}
-              onChange={onChange}
-            />
-          </div>
-
-          <div className="form-group social-input">
-            <i className="fab fa-linkedin fa-2x" />
-            <input
-              type="text"
-              placeholder="Linkedin URL"
-              name="linkedin"
-              value={linkedin}
-              onChange={onChange}
-            />
-          </div>
-
-          <div className="form-group social-input">
-            <i className="fab fa-instagram fa-2x" />
-            <input
-              type="text"
-              placeholder="Instagram URL"
-              name="instagram"
-              value={instagram}
-              onChange={onChange}
-            />
-          </div>
-        </Fragment>
-      )}
-
-      <input type="submit" className="btn btn-primary my-1" />
+      <input type="submit" className="btn btn-primary my-1" value="Submit" />
       <Link className="btn btn-light my-1" to="/dashboard">
         Go Back
       </Link>
