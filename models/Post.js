dd265cf1d90ab57313b5d6befc731b45026199ef
Theodore <thodorispliakas@gmail.com>
2020-09-15 01:08:44 +0300

    Line separators fix

--- models/Post.js
+++ models/Post.js
@@ -2,51 +2,51 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const PostSchema = new Schema({
-    user: {
+  user: {
+    type: Schema.Types.ObjectId
+  },
+  text: {
+    type: String,
+    required: true
+  },
+  name: {
+    type: String
+  },
+  avatar: {
+    type: String
+  },
+  likes: [
+    {
+      user: {
         type: Schema.Types.ObjectId
-    },
-    text: {
+      }
+    }
+  ],
+  comments: [
+    {
+      user: {
+        type: Schema.Types.ObjectId
+      },
+      text: {
         type: String,
         required: true
-    },
-    name: {
+      },
+      name: {
         type: String
-    },
-    avatar: {
+      },
+      avatar: {
         type: String
-    },
-    likes: [
-        {
-            user: {
-                type: Schema.Types.ObjectId
-            }
-        }
-    ],
-    comments: [
-        {
-            user: {
-                type: Schema.Types.ObjectId
-            },
-            text: {
-                type: String,
-                required: true
-            },
-            name: {
-                type: String
-            },
-            avatar: {
-                type: String
-            },
-            date: {
-                type: Date,
-                default: Date.now
-            }
-        }
-    ],
-    date: {
+      },
+      date: {
         type: Date,
         default: Date.now
+      }
     }
+  ],
+  date: {
+    type: Date,
+    default: Date.now
+  }
 });
 
 module.exports = mongoose.model('post', PostSchema);
