2bc3aec9a485d1667f5c12d2bd64964ff58d6eac
Theodore <thodorispliakas@gmail.com>
2020-09-16 01:17:33 +0300

    Display profiles list

---
+++ client/src/components/profiles/Profiles.js
@@ -0,0 +1,47 @@
+import React, { useEffect } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import Spinner from '../layout/Spinner';
+import ProfileItem from './ProfileItem';
+import { getProfiles } from '../../actions/profile';
+
+const Profiles = ({ getProfiles, profile: { profiles, loading } }) => {
+  useEffect(() => {
+    getProfiles();
+  }, []);
+
+  return (
+    <>
+      {loading ? (
+        <Spinner tip="Loading..." size="large" />
+      ) : (
+        <>
+          <h1 className="large text-primary">Profiles</h1>
+          <p className="lead">
+            <i className="fas fa-users" /> Browse and connect with other users.
+          </p>
+          <div className="profiles">
+            {profiles.length > 0 ? (
+              profiles.map((profile) => (
+                <ProfileItem key={profile._id} profile={profile} />
+              ))
+            ) : (
+              <h4>No profiles found.</h4>
+            )}
+          </div>
+        </>
+      )}
+    </>
+  );
+};
+
+Profiles.propTypes = {
+  getProfiles: PropTypes.func.isRequired,
+  profile: PropTypes.object.isRequired
+};
+
+const mapStateToProps = (state) => ({
+  profile: state.profile
+});
+
+export default connect(mapStateToProps, { getProfiles })(Profiles);
