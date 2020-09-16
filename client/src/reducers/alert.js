8603b13bee48610ff7012c7a0851e81517b9a758
Theodore <thodorispliakas@gmail.com>
2020-09-17 01:01:48 +0300

    Fixed several warnings

--- client/src/reducers/alert.js
+++ client/src/reducers/alert.js
@@ -6,9 +6,9 @@ export default function (state = initialState, action) {
   const { type, payload } = action;
 
   switch (type) {
-    case 'SET_ALERT':
+    case SET_ALERT:
       return [...state, payload];
-    case 'REMOVE_ALERT':
+    case REMOVE_ALERT:
       return state.filter((alert) => alert.id !== payload);
     default:
       return state;
