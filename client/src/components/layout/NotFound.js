bec7150f567118149dab771ada81737096508643
Theodore <thodorispliakas@gmail.com>
2020-09-21 00:11:56 +0300

    Changed not found page

--- client/src/components/layout/NotFound.js
+++ client/src/components/layout/NotFound.js
@@ -1,13 +1,24 @@
 import React from 'react';
+import { Result } from 'antd';
+import { Link } from 'react-router-dom';
 
 const NotFound = () => {
   return (
-    <>
-      <h1 className="x-large text-primary">
-        <i className="fas fa-exclamation-triangle" /> Page Not Found
-      </h1>
-      <p className="large">Sorry, this page does not exist</p>
-    </>
+    <Result
+      status="404"
+      l
+      title={
+        <h1 className="x-large text-primary">
+          <i className="fas fa-exclamation-triangle" /> Page Not Found
+        </h1>
+      }
+      subTitle="Sorry, the page you visited does not exist."
+      extra={
+        <Link to="/" className="btn btn-primary my-1">
+          Back Home
+        </Link>
+      }
+    />
   );
 };
 
