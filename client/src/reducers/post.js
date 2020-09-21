b8a6a4ecdca5478fc06a8be4bc30c13d3debbba7
Theodore <thodorispliakas@gmail.com>
2020-09-22 01:10:36 +0300

    Create post functionality

--- client/src/reducers/post.js
+++ client/src/reducers/post.js
@@ -2,7 +2,8 @@ import {
   DELETE_POST,
   GET_POSTS,
   POST_ERROR,
-  UPDATE_LIKES
+  UPDATE_LIKES,
+  ADD_POST
 } from '../actions/types';
 
 const initialState = {
@@ -20,6 +21,12 @@ export default function (state = initialState, action) {
       return {
         ...state,
         posts: payload,
+        loading: false
+      };
+    case ADD_POST:
+      return {
+        ...state,
+        posts: [payload, ...state.posts],
         loading: false
       };
     case DELETE_POST:
