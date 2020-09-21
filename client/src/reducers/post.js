fc7325f3051bd3c94a4daaab024cca365743b337
Theodore <thodorispliakas@gmail.com>
2020-09-22 01:49:09 +0300

    Refactored alerts. Added comments functionality

--- client/src/reducers/post.js
+++ client/src/reducers/post.js
@@ -1,9 +1,12 @@
 import {
   DELETE_POST,
   GET_POSTS,
+  GET_POST,
   POST_ERROR,
   UPDATE_LIKES,
-  ADD_POST
+  ADD_POST,
+  ADD_COMMENT,
+  REMOVE_COMMENT
 } from '../actions/types';
 
 const initialState = {
@@ -21,6 +24,12 @@ export default function (state = initialState, action) {
       return {
         ...state,
         posts: payload,
+        loading: false
+      };
+    case GET_POST:
+      return {
+        ...state,
+        post: payload,
         loading: false
       };
     case ADD_POST:
@@ -52,6 +61,23 @@ export default function (state = initialState, action) {
               }
             : post
         ),
+        loading: false
+      };
+    case ADD_COMMENT:
+      return {
+        ...state,
+        post: { ...state.post, comments: payload },
+        loading: false
+      };
+    case REMOVE_COMMENT:
+      return {
+        ...state,
+        post: {
+          ...state.post,
+          comments: state.post.comments.filter(
+            (comment) => comment._id !== payload
+          )
+        },
         loading: false
       };
     default:
