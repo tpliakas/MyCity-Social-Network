853eacc112b99249ead4c4c28c936e90117aaf81
Theodore <thodorispliakas@gmail.com>
2020-09-22 01:49:09 +0300

    Refactored alerts. Added comments functionality

--- client/src/components/layout/Alert.js
+++ client/src/components/layout/Alert.js
@@ -1,20 +1,25 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { Alert as AntdAlert } from 'antd';
+import { Alert as AntdAlert, notification } from 'antd';
 
 const Alert = ({ alerts }) =>
   alerts !== null &&
   alerts.length > 0 &&
-  alerts.map((alert) => (
-    <AntdAlert
-      message={alert.alertType}
-      description={alert.msg}
-      type={alert.alertType}
-      key={alert.id}
-      showIcon
-    />
-  ));
+  alerts.map((alert) =>
+    notification.open({
+      description: (
+        <AntdAlert
+          message={alert.alertType}
+          description={alert.msg}
+          type={alert.alertType}
+          key={alert.id}
+          showIcon
+        />
+      ),
+      placement: `bottomRight`
+    })
+  );
 
 Alert.propTypes = {
   alerts: PropTypes.array.isRequired
--- client/src/components/routing/Routes.js
+++ client/src/components/routing/Routes.js
@@ -9,6 +9,7 @@ import AddEducation from '../profile-forms/AddEducation';
 import Profiles from '../profiles/Profiles';
 import Profile from '../profile/Profile';
 import Posts from '../posts/Posts';
+import Post from '../post/Post';
 import NotFound from '../layout/NotFound';
 import PrivateRoute from '../routing/PrivateRoute';
 import CreateProfile from '../profile-forms/CreateProfile';
@@ -29,6 +30,7 @@ const Routes = (props) => {
         <PrivateRoute exact path="/add-experience" component={AddExperience} />
         <PrivateRoute exact path="/add-education" component={AddEducation} />
         <PrivateRoute exact path="/posts" component={Posts} />
+        <PrivateRoute exact path="/posts/:id" component={Post} />
         <Route component={NotFound} />
       </Switch>
     </section>
