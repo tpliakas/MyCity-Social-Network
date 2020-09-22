e25b3d423b14259d4c20c3ab0c21a5a323efbf5e
Theodore <thodorispliakas@gmail.com>
2020-09-22 03:09:52 +0300

    More testing

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 connectDB();
 
 // Init Middleware. Helps to get body data in request
-app.use(express.json({ extended: false }));
+app.use(express.json());
 
 // Define routes
 app.use('/api/users', require('./routes/api/users'));
@@ -19,16 +19,10 @@ app.use('/api/posts', require('./routes/api/posts'));
 // Serve static assets in production
 if (process.env.NODE_ENV === 'production') {
   // Set static folder
-  app.use(express.static(path.join(__dirname, './client/build')));
+  app.use(express.static('client/build'));
 
-  app.get('*', function (_, res) {
-    res.sendFile(path.join(__dirname, './client/build/index.html'), function (
-      err
-    ) {
-      if (err) {
-        res.status(500).send(err);
-      }
-    });
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 }
 
