ec7f65b0d074af085b22e87df9c61679aeb6a28f
Theodore <thodorispliakas@gmail.com>
2020-09-22 03:04:00 +0300

    More testing

--- server.js
+++ server.js
@@ -19,10 +19,16 @@ app.use('/api/posts', require('./routes/api/posts'));
 // Serve static assets in production
 if (process.env.NODE_ENV === 'production') {
   // Set static folder
-  app.use(express.static('./client/build'));
+  app.use(express.static(path.join(__dirname, './client/build')));
 
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, './client/build/index.html'));
+  app.get('*', function (_, res) {
+    res.sendFile(path.join(__dirname, './client/build/index.html'), function (
+      err
+    ) {
+      if (err) {
+        res.status(500).send(err);
+      }
+    });
   });
 }
 
