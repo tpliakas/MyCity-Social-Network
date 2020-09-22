888730d681e4abbe3077b6f416d40ed226c686f4
Theodore <thodorispliakas@gmail.com>
2020-09-23 02:23:47 +0300

    Changed notification to message

--- client/src/components/layout/Alert.js
+++ client/src/components/layout/Alert.js
@@ -1,24 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { Alert as AntdAlert, notification } from 'antd';
+import { message } from 'antd';
 
 const Alert = ({ alerts }) =>
   alerts !== null &&
   alerts.length > 0 &&
   alerts.map((alert) =>
-    notification.open({
-      description: (
-        <AntdAlert
-          message={alert.alertType}
-          description={alert.msg}
-          type={alert.alertType}
-          key={alert.id}
-          showIcon
-        />
-      ),
-      placement: `bottomRight`
-    })
+    alert.alertType === 'success'
+      ? message.success(alert.msg)
+      : alert.alertType === 'error'
+      ? message.error(alert.msg)
+      : alert.alertType === 'danger'
+      ? message.warning(alert.msg)
+      : message.info(alert.msg)
   );
 
 Alert.propTypes = {
