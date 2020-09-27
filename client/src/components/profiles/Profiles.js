8872013c97125b8ad3156ece80243088e7cf2626
Theodore <thodorispliakas@gmail.com>
2020-09-27 03:18:14 +0300

    Logo, manifest, new navbar style and landing page

--- client/src/components/profiles/Profiles.js
+++ client/src/components/profiles/Profiles.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { Input } from 'antd';
-import { motion } from 'framer-motion';
+import { motion, AnimatePresence } from 'framer-motion';
 import Spinner from '../layout/Spinner';
 import ProfileItem from './ProfileItem';
 import { getProfiles } from '../../actions/profile';
@@ -11,10 +11,16 @@ const { Search } = Input;
 
 const Profiles = ({ getProfiles, profile: { profiles, loading } }) => {
   const [searchValue, setSearchValue] = useState(null);
+  const [hasProfiles, setHasProfiles] = useState(null);
 
   useEffect(() => {
     getProfiles();
   }, [getProfiles]);
+
+  useEffect(() => {
+    if (!profiles) return;
+    setHasProfiles(profiles.length);
+  }, [profiles]);
 
   const onSearch = (value) => setSearchValue(value);
 
@@ -37,44 +43,47 @@ const Profiles = ({ getProfiles, profile: { profiles, loading } }) => {
             />
           </div>
           <div className="profiles">
-            {profiles.length > 0 ? (
-              profiles.map((profile) => {
-                if (searchValue) {
-                  if (
-                    profile.user.name
-                      .toLowerCase()
-                      .includes(searchValue.toLowerCase())
-                  )
+            {hasProfiles > 0
+              ? profiles.map((profile) => {
+                  if (searchValue) {
+                    if (
+                      profile.user.name
+                        .toLowerCase()
+                        .includes(searchValue.toLowerCase())
+                    )
+                      return (
+                        <motion.div
+                          key={profile._id}
+                          initial={{ x: '-500px', opacity: 0 }}
+                          animate={{
+                            x: 0,
+                            opacity: 1,
+                            transition: { duration: 0.8 }
+                          }}
+                          exit={{ opacity: 0, scale: 0 }}
+                        >
+                          <ProfileItem key={profile._id} profile={profile} />
+                        </motion.div>
+                      );
+                  } else {
                     return (
-                      <motion.div
-                        initial={{ x: '-500px', opacity: 0 }}
-                        animate={{
-                          x: 0,
-                          opacity: 1,
-                          transition: { duration: 0.8 }
-                        }}
-                      >
-                        <ProfileItem key={profile._id} profile={profile} />
-                      </motion.div>
+                      <AnimatePresence key={profile._id}>
+                        <motion.div
+                          initial={{ x: '-500px', opacity: 0 }}
+                          animate={{
+                            x: 0,
+                            opacity: 1,
+                            transition: { duration: 0.8 }
+                          }}
+                          exit={{ opacity: 0, scale: 0 }}
+                        >
+                          <ProfileItem key={profile._id} profile={profile} />
+                        </motion.div>
+                      </AnimatePresence>
                     );
-                } else {
-                  return (
-                    <motion.div
-                      initial={{ x: '-500px', opacity: 0 }}
-                      animate={{
-                        x: 0,
-                        opacity: 1,
-                        transition: { duration: 0.8 }
-                      }}
-                    >
-                      <ProfileItem key={profile._id} profile={profile} />
-                    </motion.div>
-                  );
-                }
-              })
-            ) : (
-              <h4>No profiles found.</h4>
-            )}
+                  }
+                })
+              : hasProfiles === 0 && <h4>No profiles found.</h4>}
           </div>
         </>
       )}
