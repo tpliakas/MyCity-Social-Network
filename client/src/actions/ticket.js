7ca8b91a9d6318a68fdf4a669b0e27eab2050669
Theodore <thodorispliakas@gmail.com>
2020-11-28 18:32:40 +0200

    Wip issue ticket form component

--- client/src/actions/ticket.js
+++ client/src/actions/ticket.js
@@ -1,6 +1,5 @@
 import axios from 'axios';
-import { GET_TICKETS, GET_TICKET, ADD_TICKET, TICKET_ERROR } from './types';
-import { setAlert } from './alert';
+import { GET_TICKETS, GET_TICKET, TICKET_ERROR } from './types';
 
 // Get tickets
 export const getTickets = () => async (dispatch) => {
@@ -28,31 +27,6 @@ export const getTicket = (id) => async (dispatch) => {
       type: GET_TICKET,
       payload: res.data
     });
-  } catch (err) {
-    dispatch({
-      type: TICKET_ERROR,
-      payload: { msg: err.response.statusText, status: err.response.status }
-    });
-  }
-};
-
-// Add ticket
-export const addTicket = (formData) => async (dispatch) => {
-  const config = {
-    headers: {
-      'Content-Type': 'application/json'
-    }
-  };
-
-  try {
-    const res = await axios.post('/api/ticket', formData, config);
-
-    dispatch({
-      type: ADD_TICKET,
-      payload: res.data
-    });
-
-    dispatch(setAlert('New Ticket Issue Created', 'success'));
   } catch (err) {
     dispatch({
       type: TICKET_ERROR,
